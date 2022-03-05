5bc046300f1ae10619cdea500ea1cc6375c17f58
raphael-khan <raphaelkhan@yahoo.com>
2022-03-05 11:56:33 -0500

    completed the web scrapper project

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # web_scrapper_project
 
 -> Web scrapping Hacker News using Beautiful Soup.
+    Loops through the titlelink and subtext classes and extracts the text, links and votes. Runs an IF statement to check if the votes are higher than 99 and returns a sorted dictionary. 
+
+    Dictionary is sorted by the number of votes in an increasing order using a Lambda function. 
--- scrapper.py
+++ scrapper.py
@@ -1,16 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 response = requests.get('https://news.ycombinator.com/news')
 
 soup = BeautifulSoup(response.text, 'html.parser')
 # print(soup.prettify())
 
-links = print(soup.select('.titlelink')[0])
+links = soup.select('.titlelink')
 
-votes = print(soup.select('.score')[0])
+subtext = soup.select('.subtext')
 
-def create_custom_news(links, votes):
+def sort_stories_by_votes(news): 
+    return sorted(news, key=lambda k:k['votes'] , reverse=True)  # sorting a dictionary by votes in increasing order. 
+
+## Function that gets us the title, links and score of every post on the newsfeed ##
+def create_custom_news(links, subtext):
     news = []
-    for 
-    return news
+    for  idx, item in enumerate(links):
+        title = links[idx].getText()         # gets us the text or title in this case. 
+        href = links[idx].get('href', None)   # None in case if a post does not have a link. 
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                news.append({'title': title, 'link': href, 'votes': points})    # appends a dictionary  to the list since we have more than one variable. 
+    return sort_stories_by_votes(news)
+
+pprint.pprint(create_custom_news(links, subtext))
