da5012839a1bead987141712df0bd89f8ad025b3
raphael-khan <raphaelkhan@yahoo.com>
2022-03-04 19:35:18 -0500

    created a soup object

--- scrapper.py
+++ scrapper.py
@@ -2,4 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://news.ycombinator.com/news')
-print(response.text)
+# print(response.text)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify())
+print(soup.body.contents)  # returns a list
+print(soup.findAll('a'))
+print(soup.find(id='score_30560573'))
+print(soup.a)
+print(soup.title)
+
